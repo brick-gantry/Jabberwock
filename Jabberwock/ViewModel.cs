68f0d23ff14336e5583e30a1b01572e97ac11b8a
mudbug <mahlok@gmail.com>
2015-09-22 22:49:58 -0500

    front end

---
+++ Jabberwock/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jabberwock
+{
+    public class ViewModel
+    {
+        public int SelectedProfileIndex { get; set; }
+        private ObservableCollection<Profile> profiles = new ObservableCollection<Profile>();
+        public ObservableCollection<Profile> Profiles { get { return profiles; } }
+
+        public Profile SelectedProfile { get { return profiles[SelectedProfileIndex]; } }
+    }
+}
